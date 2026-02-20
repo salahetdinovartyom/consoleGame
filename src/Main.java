9ad56424a3731b4c3651339da0ab2f8f2bae097f
salahetdinovartyom <210269813+salahetdinovartyom@users.noreply.github.com>
2026-02-20 23:26:40 +0300

    vstrecha3 update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,78 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        byte step=0;
+        short personX;
+        short personY;
+        byte personLive=3;
+        byte sizeBoard=5;
+       /*personX=1+sizeBoard/2;
+       personY=1+sizeBoard/2;
+       step+=1; */
+        String person = "🦸🏻";
+        String monster = "👹";
+        String gamingField= "+ —— + —— + —— +\n"
+                + "|    |    |    |\n"
+                + "+ —— + —— + —— +\n"
+                + "|    | " + monster + " |    |\n"
+                + "+ —— + —— + —— +\n"
+                + "| " + person + " |    |    |\n"
+                + "+ —— + —— + —— +";
+        System.out.println("Привет! Ты готов начать играть в игру? (Напиши: ДА или НЕТ)");
+        Scanner scanner= new Scanner(System.in);
+        String answer=scanner.nextLine();
+        System.out.println("Ваш ответ:\t" + answer );
+        System.out.println("Кол-во жизней:"+ personLive+ " "+ person);
+        System.out.println(gamingField);
+//        Scanner in = new Scanner(System.in);
+//        double d=in.nextDouble();
+//        System.out.println(d);
+//        ВСТРЕЧА 3
+//        int personX = 1;
+//        int personY = 3;
+//        System.out.println(personX > 10); //выведет false
+//        System.out.println(personY == 3); //выведет true
+        switch (answer) {
+            case "ДА":
+                System.out.println("Начинаем играть!"); /*Начинаем доклад октонавты*/
+                System.out.println("Выбери сложность игры (от 1 до 5): ");
+                byte difficultGame =scanner.nextByte();
+                System.out.println("Выбранная сложность:\t" + difficultGame);
+                break;
+            case "НЕТ":
+                System.out.println("У меня вообще-то супер пупер мега игра где можно выиграть миллион(((");
+                System.out.println("Ну ладно если захочешь потом приходи!");
+                break;
+            default:
+                System.out.println("ДА иль НЕТ дай мне ответ!");
+
+
+        }
+        personX=3;
+        personY=1;
+        System.out.println("Введите куда пойдёт персонаж (ход возможен только по вертикали или горизонтали)");
+        System.out.printf("Координаты персонажа - x: "+ personX +", y:" +personY);
+        byte x=scanner.nextByte();
+        byte y=scanner.nextByte();
+        if (x != personX && y!=personY) {
+            System.out.println("Некорректный ход");
+        }
+//        Math.abs(x - personX);
+        if (x!=personX && y != personY) {
+            System.out.println("Некорректный ход");
+        }
+        else if (Math.abs(x - personX)==1 || Math.abs(y-personY)==1) {
+            personX=x;
+            personY=y;
+            step+=1;
+            System.out.println("Ход корректный. Новые координаты: "+ personX + ", " + personY + "\nХод номер: " + step);
+        }
+        else {
+            System.out.println("Координаты не изменены");
+        }
+
+
+
     }
 }
--- src/Vstrecha2.java
+++ src/Vstrecha2.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Vstrecha2 {
+import java.util.Scanner;
+
+public class Vstrecha2 {
+    public static void main (String[] args){
+        Scanner in=new Scanner(System.in);
+        int a=in.nextInt();
+        int b=in.nextInt();
+        if (a>0 || b>0) {
+            System.out.println("положительное число");
+        }
+        else if (a==0) {
+            System.out.println("Вы ввели 0");
+        }
+        else {
+            System.out.println("отрицательное число");
+        }
+        switch (a){
+            case 0:
+                System.out.println("Вы ввели 0");
+                break;
+            case 1,2:
+                a+=1;
+                break;
+
+        }
+        System.out.println(a);
+    }
+
+
 }
